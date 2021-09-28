30a89bd9bad9c78ce2ff646206fdde298b78bf4f
lioyan.top <8372272+lioyantop@user.noreply.gitee.com>
2021-09-28 16:37:34 +0800

    Initial commit

---
+++ jul/src/main/java/com/lioyan/jul/Test1.java
@@ -0,0 +1,36 @@
+package com.lioyan.jul;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+/**
+ * {@link Test1}
+ *
+ * @author com.lioyan
+ * @date 2021/9/28  13:50
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        LogManager manager = LogManager.getLogManager();
+        System.setProperty("java.util.logging.config.file", "C:\\work\\idea_work\\java-log\\jul\\src\\main\\resources\\logging.properties");
+        Logger logger = Logger.getLogger("com.leon.JULTest");
+        Logger logger2 = Logger.getLogger("com.leon");
+        Logger logger3 = Logger.getLogger("com");
+        // 2.日志记录输出
+        logger.info("hello jul");
+
+        // 通用方法进行日志记录
+        logger.log(Level.INFO, "info msg");
+
+
+        // 通过占位符 方式输出变量值
+        String name = "leon";
+        Integer age = 13;
+        logger.log(Level.INFO, "用户信息：{0},{1}", new Object[]{name, age});
+
+
+    }
+
+}
